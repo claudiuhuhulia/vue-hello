ae0a89332b409b5030add5cb39e88028796b248a
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-16 16:35:37 +0200

    bonus

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('vueok', Vue)
+
+const { createApp } = Vue;
+
+const app = createApp ({
+    data() {
+        return {
+            name: 'Cloud',
+            pic:"https://citynews-milanotoday.stgy.ovh/~media/horizontal-mid/52228367014708/una-pantera-nera-2.jpg",
+        }
+    }
+
+})
+
+app.mount('#root');
